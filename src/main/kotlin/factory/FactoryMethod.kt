ad7c09340d4b751d708e62a312d45748f549fa84
schellenberg <daniel.schellenberg@siginifico.com>
2025-06-12 11:26:15 +0200

    feat(factory method): implementing a factory method example using teas and tea farms

--- src/main/kotlin/factory/FactoryMethod.kt
+++ src/main/kotlin/factory/FactoryMethod.kt
@@ -1,61 +1,64 @@
 package factory
 
-
-abstract class Tea{
-    abstract val name: String
-    abstract val variant: String
-    abstract val temp: Int
+interface Tea {
+    val name: String
+    fun steep()
 }
 
-data class GreenTea(
-    override val name : String,
-    override val variant: String, 
-    override val temp: Int
-): Tea()
-data class BlackTea(
-    override val name : String,
-    override val variant: String, 
-    override val temp: Int
-): Tea()
-
-
-
-interface TeaHouse { 
-    
-    fun steamTea(name: String): Tea
-
-    fun serveTea(name: String) {
-        val tea = steamTea(name)
-        
-        println("Served ${tea.name} of the ${tea.variant} variant.")
+private class GreenTea(override val name: String) : Tea {
+    override fun steep() {
+        println("Steep at 60°C")
     }
 }
 
-class GreenTeaSteamer(): TeaHouse {
-    override fun steamTea(name: String): Tea {
-        return GreenTea(
-            name = name,
-            variant = "Dragonwell",
-            temp = 70,
-        ) 
+private class BlackTea(override val name: String) : Tea {
+    override fun steep() {
+        println("Steep at 100°C")
     }
 }
 
-class BlackTeaSteamer(): TeaHouse {
-    override fun steamTea(name: String): Tea {
-        return BlackTea(
-            name = name,
-            variant = "Assam",
-            temp = 70,
-        )
+abstract class TeaFarm(
+    val name: String,
+    val country: String
+) {
+    abstract val teas: MutableList<Tea>
+
+    abstract fun produceTea(teaName: String): Tea
+    open fun serveSample(tea: Tea) {
+        if (teas.contains(tea)) {
+            tea.steep()
+            println("Sample of ${tea.name} served from $name tea farm in $country:")
+            println("Serving hot ${tea.name}")
+        } else {
+            println("${tea.name} is not produced here")
+        }
     }
+}
+
+class GreenTeaFarm(name: String, country: String) : TeaFarm(name, country) {
+    override val teas: MutableList<Tea> = mutableListOf()
+    override fun produceTea(teaName: String): Tea = GreenTea(name = teaName).also { tea -> teas.add(tea) }
+}
+
+class BlackTeaFarm(name: String, country: String) : TeaFarm(name, country) {
+    override val teas: MutableList<Tea> = mutableListOf()
+    override fun produceTea(teaName: String): Tea = BlackTea(name = teaName).also { tea -> teas.add(tea) }
 }
 
 fun main() {
-    val greenTeaSteamer = GreenTeaSteamer()
-    val blackTeaSteamer = BlackTeaSteamer()
-    
-    greenTeaSteamer.serveTea("Green tea of the week")
-    blackTeaSteamer.serveTea("Black tea of the week")
-    
+    val gtf = GreenTeaFarm(
+        name = "Steamy heaven",
+        country = "China"
+    )
+
+    val btf = BlackTeaFarm(
+        name = "Home of Assam",
+        country = "India"
+    )
+
+    gtf.produceTea("LongJing")
+    btf.produceTea("Assam")
+
+    gtf.teas.forEach { gtf.serveSample(it) }
+    btf.teas.forEach { gtf.serveSample(it) }
 }
